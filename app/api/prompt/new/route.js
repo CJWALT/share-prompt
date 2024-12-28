a4823ac38257cfee3d72c85cba5f4cda00e45192
cjwalt <oyedelee22@gmail.com>
2024-12-27 22:34:34 -0800

    update

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -15,6 +15,9 @@ export const POST = async (req) =>{
         })
 
         await newPrompt.save();
+
+        await res.revalidate('/feed');
+        
         return new Response(JSON.stringify(newPrompt), { 
             status: 201 })
     } catch(err){ 
