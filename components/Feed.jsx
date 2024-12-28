b8f984513ae6a058f4f27af524cff1a13fc0f123
cjwalt <oyedelee22@gmail.com>
2024-12-28 00:46:29 -0800

    updates

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -32,8 +32,9 @@ const Feed = () => {
 
 
   const fetchPosts = async ()=> {
-    const response = await fetch('/api/prompt')
+    const response = await fetch('/api/prompt', {cache:'no-store'})
     const data = await response.json();
+
     setAllPosts(data)
     
   }
